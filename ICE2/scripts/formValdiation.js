e082bd987658ff4ec6f16dcb65cb0e8ae5d120c3
BERNIE WARREN <bernie.warren@dcmail.ca>
2024-01-27 16:14:03 -0500

    Updating formValidation.js
    Creating a username validation function with a regex borrowed (and cited) from stackoverflow. Using ==== symbols to mark off TODO items that are completed at this stage.

--- ICE2/scripts/formValdiation.js
+++ ICE2/scripts/formValdiation.js
@@ -36,13 +36,39 @@ function validateEmailAddressRegex(emailString) {
 		&& emailString.match(emailRegex);
 }
 
+function validateUsernameRegex(usernameString) {
+    // Using Regex and information found on Stackoverflow.com 
+    // In response to user learning how to use regex, user Alan Moore responded with 
+    // a series of Regex lines and explainations of their use and tips/tricks. 
+    // https://stackoverflow.com/questions/1221985/how-to-validate-a-user-name-with-regex
+    // I'll be using the first one in his list of responses, as it is the simplest and 
+    // most akin to my needs for simple username vaildation.
+
+    var usernameRegex = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/; // Allows only ASCII characters with hyphens, underscores, and spaces.
+                                                                // Does not allow separators for 1st/last characters.
+                                                                // Does not allow 2 separators in a row (forces char after separator).
+
+    // Same logic as email, if username is a valid string that matches the regex, return true                                                            
+    if (!!usernameString && typeof usernameString === 'string'
+        && usernameString.match(usernameRegex)) {
+        return true;     
+    } else {
+        // If the username does not match the regex or is not a valid string, 
+        // Change the general error message, highlight it in red and return false. 
+        var targetElement = document.getElementById("generalError");
+        targetElement.textContent = "Error - invalid Username. Try something else.";
+        targetElement.style.color = "red";
+        return false; 
+    }
+}
+
 
 //TODO:
-// Make all fields required (HTML)
-// Add a pattern attribute in the telephone element (HTML)
-// Ensure the password is redacted into dots on screen when typed in (HTML)
-// Create a function that knows if the username is valid (feel free to grab some RegEx from the Interwebs - cite source and test it!)
-// If it is not valid, be sure to update the #generalError <p></p> with a good error message and some highlighting (red background, maybe?)
+// Make all fields required (HTML)                                                      ==========================
+// Add a pattern attribute in the telephone element (HTML)                              ==========================
+// Ensure the password is redacted into dots on screen when typed in (HTML)             ===========================
+// Create a function that knows if the username is valid (feel free to grab some RegEx from the Interwebs - cite source and test it!)       ========================
+// If it is not valid, be sure to update the #generalError <p></p> with a good error message and some highlighting (red background, maybe?) ========================
 // Validate the username when the form is submitted
 // Clear any additional error message and highlighting when the form is reset
 // COMMENT EVERYTHING :D
