086c94688445885e3bd7fad7a4a752ea32b11799
BERNIE WARREN <bernie.warren@dcmail.ca>
2024-01-30 18:40:32 -0500

    ICE 3 Filtering

--- ICE3/scripts/selectors.js
+++ ICE3/scripts/selectors.js
@@ -29,6 +29,21 @@ function traversingTheDOM() {
     $('#list').next().css('background-color', "#fcf30040");
 
     $('#list').find('li').css('background-color', "#fcf300040");
-}
+    // Traverse up one level 
+    $('#list').parent('div').css('font-size', "24px");
+    // Traverse all the way up to the body through all parents, grandparents etc. 
+    $('#list').parents('body').css('font-size', "24px");
+}   
 
-cssSelectors();
+function filtering() {
+    // Short version of the same thing below 
+    $('#list').find('li').filter(':even').css('background-color', "#2a9d8f");
+
+    // Long version of the same thing above ^^^
+    $('#list').find('li').filter(function (i) {
+        return i % 2 == 0;
+    }).css('background-color','red');
+
+}
+filtering();
+// cssSelectors();
