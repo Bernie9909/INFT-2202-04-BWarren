b88f46a3c81058619ef8f12d22e91589a0440a4e
BERNIE WARREN <bernie.warren@dcmail.ca>
2024-02-20 18:19:05 -0500

    ICE 6 Updates

--- ICE6/scripts/xhr_script.js
+++ ICE6/scripts/xhr_script.js
@@ -1,19 +1,37 @@
 console.log("xhr_script.js loaded");
 
 // get the button for xhr
+let btn_Xhr = $('#xhrJoke');
 
 // create a url variable
+let url_Xhr = "https://icanhazdadjoke.com/";
 
 // create a click callback that handles the API call
+$(btn_Xhr).click(() => {
     // instantiate an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
     // confiture with open()
+    xhr.open("GET", url_Xhr);
     // set the necessary headers for icanhazdadjoke.com
+    xhr.setRequestHeader("Accept", "application/json");
     // create the callback to handle the onreadystatechange
+    xhr.onreadystatechange = function () {
         // only execute when done and status is 200
-            // get the responseText
-            // console log
-            // JSON.parse the joke
-            // set the output
-    // send the request
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
 
+        
+        // get the responseText
+        let joke = xhr.responseText;
+        // console log
+        console.log(joke);
+        // JSON.parse the joke
+        let convertedJoke = JSON.parse(joke);
+        // set the output (target the p not the div)
+        $('#output').text(convertedJoke.responseText);
+        }
+    }
+        
+    // send the request
+    xhr.send();
+});
 
