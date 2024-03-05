11a4e1e636428d99fc44c02e1cff036ae744b14c
BERNIE WARREN <bernie.warren@dcmail.ca>
2024-03-05 18:05:47 -0500

    ICE7 In class update

--- ICE7/scripts/pixabay.js
+++ ICE7/scripts/pixabay.js
@@ -4,10 +4,15 @@
 console.log('pixabay.js loaded');
 
 // API Key for Pixabay
-const PIXABAY_API_KEY = ''; // normally we would load a key through the .env file
+// normally we would load a key through the .env file
+const PIXABAY_API_KEY = '42719380-662aa52e23b2caef87a34807f';
 // URL for Pixbay request
 const PIXABAY_URL = 'https://pixabay.com/api/?key=<API_KEY>';
 // Constant for image count
+const IMAGE_COUNT = 30;
+
+// https://pixabay.com/api/?key=42719380-662aa52e23b2caef87a34807f&per_page=30
+
 
 
 /**
@@ -16,6 +21,25 @@ const PIXABAY_URL = 'https://pixabay.com/api/?key=<API_KEY>';
  */
 const makePosts = () => 
 {
+    getPictures();
+    const blogColumn = $('.blog-column');
+    for (let i = 0; i<IMAGE_COUNT; i++) {
+        let id = i;
+        let card = $('div class="card">hi</div>')
+        .attr("id", "photo_" + id)
+        .appendTo(blogColumn);
+
+        let pic = $('<img>')
+        .attr('id', "img-"+1)
+        .addClass('card-img-top')
+        // Append pic to card
+        .appendTo(card)
+        let cardBody = $('<div class ="card-body"></div>');
+        // Append card-body to the card elemtn 
+        
+        let cardParagraph = $('<p class = "card-text tags"></div>');
+        // Append paragraph to card body 
+    }
     // make HTML elements for posts using jQuery, same number as images retrieving
         // use bootstrap cards and append to
         // i.e. https://getbootstrap.com/docs/5.2/components/card/#about
@@ -24,7 +48,7 @@ const makePosts = () =>
         // card title
         // image
         // card text
-
+        // use tags for img alt= and card text 
 };
 
 /**
@@ -33,7 +57,25 @@ const makePosts = () =>
  */
 const getPictures = () => 
 {
+    // Get images matching the following: 30 per page, query value of cars, all horizontal ONLY (no vert), type photo only. 
+    const url = `${PIXABAY_URL.replace('<API_KEY>', PIXABAY_API_KEY)}&q=cars&orientation=horizontal&image_type=photo&per_page=${IMAGE_COUNT}`;
     // use fetch to get the pictures from the API
+    fetch(url)
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data => {
+        console.log(data);
+        console.log(data?.hits);
+        // Data retrieved 
+        //create posts
+        // makePosts();
+    }))
+    .catch((err) => {
+        // log to console 
+        console.log(err);
+    })
     // handle error(s) with .catch()
 };
 
